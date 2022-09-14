0665ad6f2641351ae88f784ada4a8696fe904f3a
Alan Cruickshank <alan@designingoverload.com>
2022-09-14 11:53:29 +0100

    raw_segments_with_stack

--- src/sqlfluff/core/parser/segments/base.py
+++ src/sqlfluff/core/parser/segments/base.py
@@ -456,6 +456,24 @@ class BaseSegment(metaclass=SegmentMetaclass):
         return self.get_raw_segments()
 
     @cached_property
+    def raw_segments_with_stack(self) -> List[Tuple["RawSegment", List["BaseSegment"]]]:
+        """Returns a list of raw segments in this segment with the parents."""
+        buffer = []
+        for seg in self.segments:
+            # If it's a raw, yield it with this segment as the parent
+            if seg.is_type("raw"):
+                buffer.append((seg, [self]))
+            # If it's not, recurse - prepending self to the parent stack
+            else:
+                buffer.extend(
+                    [
+                        (raw_seg, [self] + stack)
+                        for raw_seg, stack in seg.raw_segments_with_stack
+                    ]
+                )
+        return buffer
+
+    @cached_property
     def source_fixes(self) -> List[SourceFix]:
         """Return any source fixes as list."""
         return list(chain.from_iterable(s.source_fixes for s in self.segments))
@@ -798,6 +816,7 @@ class BaseSegment(metaclass=SegmentMetaclass):
             "raw_upper",
             "matched_length",
             "raw_segments",
+            "raw_segments_with_stack",
             "first_non_whitespace_segment_raw_upper",
             "source_fixes",
             "full_type_set",
